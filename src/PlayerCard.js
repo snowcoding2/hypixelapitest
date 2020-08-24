cf08da1b3fe111cc46360aa5899318111a88c19f
Rafael Duarte <rafaelmorrisonduarte@gmail.com>
2020-08-24 14:21:09 +0100

    Added Playercard which contains all player information for individual searching

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import Namesearch from './Namesearch';
-import Wins from './Wins';
+import React, { Component } from 'react';
+import './PlayerCard.css';
 
-export default (props) => props.user == null ?
-          <div className="App">
-            < Namesearch handleSelectUser={this.handleSelectUser.bind(this)}/>
-          </div>
-          :
-          <div className="App">
-            < Namesearch handleSelectUser={this.handleSelectUser.bind(this)}/>
-            <h1>{user.displayname}</h1>
-            < Wins wins={props.user.stats.Bedwars.wins_bedwars} losses={props.user.stats.Bedwars.losses_bedwars} />
-          </div>
+const API = 'https://api.hypixel.net/player?key=afdf14f9-ec52-4e9f-b1dc-20176ef6ea7b&name=';
+
+class PlayerCard extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+      user: {},
+      wins: undefined,
+      losses: undefined
+    }
+  }
+
+  clicked(e) {
+    e.preventDefault();
+    fetch(API + this.state.text)
+        .then(result => result.json())
+        .then(data => this.setState({
+          user: data.player,
+          losses: data.player.stats.Bedwars.losses_bedwars,
+          wins: data.player.stats.Bedwars.wins_bedwars
+        }));
+    
+}
+
+  typing(e) {
+    this.setState({
+        text: e.target.value
+    })
+}
+
+  round(num, digits) {
+    let decimalPlaces = Math.pow(10, digits);
+    return Math.round((num + Number.EPSILON) * decimalPlaces) / decimalPlaces;
+  }
+
+  render() { 
+    const { user, wins, losses } = this.state;
+    return (  
+      <div className="player-card">
+        <form>
+            <input placeholder="Search..." onChange={this.typing.bind(this)}/>
+            <button onClick={this.clicked.bind(this)}>Select</button>
+            <h1>{user.displayName}</h1>
+            <p>Wins: {wins}</p>
+            <p>Losses: {losses}</p>
+            <p>Win percentage: {this.round((wins / (wins + losses) * 100), 2)} %</p>
+        </form>
+      </div>
+     );
+  }
+}
+ 
+export default PlayerCard;
--- src/Wins.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default (props) => <div>
-        <p>Wins: {props.wins}</p>
-        <p>Losses: {props.losses}</p>
-        <p>Winrate: {props.wins / (props.wins+props.losses) * 100}</p>
-    </div>
